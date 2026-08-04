fac7004f4d1ae3cfeb174ed7c33ae6f50f29cba9
Fundraisings <miscuartos.app@gmail.com>
2026-08-04 18:17:46 -0400

    Add button click event and console log message

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Causa30 iniciado correctamente");
+
+
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach(button => {
+
+button.addEventListener("click",()=>{
+
+alert("Gracias por ayudar a compartir esta historia ❤️");
+
+});
+
+});
