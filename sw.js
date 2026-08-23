4c588eb6117f3869bc360a93150b853a5e8d6e37
Fundraisings <miscuartos.app@gmail.com>
2026-08-23 18:49:46 -0400

    Improve fetch event handling in service worker

--- sw.js
+++ sw.js
@@ -27,6 +27,11 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, response.clone()));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
