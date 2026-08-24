799624b5d8b88f7e7ce455f0e2f9fa1604d56662
Fundraisings <miscuartos.app@gmail.com>
2026-08-24 13:37:16 -0400

    Update service worker cache version and response handling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'causa30-v1';
+const CACHE_NAME = 'causa30-v3';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -29,7 +29,8 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, response.clone()));
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
         return response;
       })
       .catch(() => caches.match(event.request))
